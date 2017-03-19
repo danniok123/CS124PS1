4a5082240e82828338abcd81365f992b84c71c24
Danielle Okezie <danielleokezie@gmail.com>
2017-03-18 20:32:17 -0400

    everything works

--- main.c
+++ main.c
@@ -5,13 +5,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include <limits.h>
 #include <unistd.h>
-#include <stdbool.h>
 #include "main.h"
 
 #define threshold 15
@@ -82,18 +78,6 @@ void freeMatrix(int d, int **a) {
     free(a);
 }
 
-// read in the test file and generate two matrices
-// 2d^2 total lines
-void graphGeneration() {
-    // read in file
-    //return false;
-}
-
-// calculate runtime of algorithm
-void runtime() {
-    //return false;
-}
-
 // Strassen's algorithm
 int **strassen(int **x, int **y, int dim) {
 
@@ -115,7 +99,7 @@ int **strassen(int **x, int **y, int dim) {
         int **h = newMatrix(half);
 
         for (int i = 0; i < half; i++) {
-            for (int j = 0; i < half; j++) {
+            for (int j = 0; j < half; j++) {
                 a[i][j] = x[i][j];
                 b[i][j] = x[i][j + half];
                 c[i][j] = x[i + half][j];
@@ -127,28 +111,28 @@ int **strassen(int **x, int **y, int dim) {
             }
         }
 
-        int **p1 = strassen(a, add_matrix(f, h, dim, -1), dim); // A(F - H)
-        int **p2 = strassen(add_matrix(a, b, dim, 1), h, dim); // (A + B)H
-        int **p3 = strassen(add_matrix(c, d, dim, 1), e, dim); // (C + D)H
-        int **p4 = strassen(d, add_matrix(g, e, dim, -1), dim); // D(G - E)
-        int **p5 = strassen(add_matrix(a, d, dim, 1), add_matrix(e, h, dim, 1), dim); // (A + D)(E + H)
-        int **p6 = strassen(add_matrix(b, d, dim, -1), add_matrix(g, h, dim, 1), dim); // (B - D)(G + H)
-        int **p7 = strassen(add_matrix(a, c, dim, -1), add_matrix(e, f, dim, 1), dim); // (A - C)(E + F)
+        int **p1 = strassen(a, add_matrix(f, h, half, -1), half); // A(F - H)
+        int **p2 = strassen(add_matrix(a, b, half, 1), h, half); // (A + B)H
+        int **p3 = strassen(add_matrix(c, d, half, 1), e, half); // (C + D)H
+        int **p4 = strassen(d, add_matrix(g, e, half, -1), half); // D(G - E)
+        int **p5 = strassen(add_matrix(a, d, half, 1), add_matrix(e, h, half, 1), half); // (A + D)(E + H)
+        int **p6 = strassen(add_matrix(b, d, half, -1), add_matrix(g, h, half, 1), half); // (B - D)(G + H)
+        int **p7 = strassen(add_matrix(a, c, half, -1), add_matrix(e, f, half, 1), half); // (A - C)(E + F)
 
         // AE + BG = P5 + P4 - P2 + P6
-        int **z11 = add_matrix(p5, add_matrix(p4, add_matrix(p6, p2, dim, -1), dim, 1), dim, 1);
+        int **z11 = add_matrix(p5, add_matrix(p4, add_matrix(p6, p2, half, -1), half, 1), half, 1);
          
         // AF + BH = P1 + P2
-        int **z12 = add_matrix(p1, p2, dim, 1);
+        int **z12 = add_matrix(p1, p2, half, 1);
         
         // CE + DG = P3 + P4
-        int **z21 = add_matrix(p3, p4, dim, 1);
+        int **z21 = add_matrix(p3, p4, half, 1);
 
         // CF + DH = P5 + P1 - P3 - P7
-        int **z22 = add_matrix(add_matrix(p5, p1, dim, 1), add_matrix(p3, p7, dim, 1), dim, -1);
+        int **z22 = add_matrix(add_matrix(p5, p1, half, 1), add_matrix(p3, p7, half, 1), half, -1);
 
         for (int i = 0; i < half; i++) {
-            for (int j = 0; i < half; j++) {
+            for (int j = 0; j < half; j++) {
                 z[i][j] = z11[i][j];
                 z[i][j + half] = z12[i][j];
                 z[i + half][j] = z21[i][j];
@@ -156,14 +140,19 @@ int **strassen(int **x, int **y, int dim) {
             }
         }
 
-        freeMatrix(dim, a); freeMatrix(dim, b); freeMatrix(dim, c); freeMatrix(dim, d);
-        freeMatrix(dim, e); freeMatrix(dim, f); freeMatrix(dim, g); freeMatrix(dim, h);
-        freeMatrix(dim, p1); freeMatrix(dim, p2); freeMatrix(dim, p3); freeMatrix(dim, p4);
-        freeMatrix(dim, p5); freeMatrix(dim, p6); freeMatrix(dim, p7); 
-
-        return z;
+    return z;
     }
+}
 
+// Pad to the nearest power of 2
+int padding(int d) {
+
+    int p = 1;
+
+    while(p < d)
+        p *= 2;
+
+    return p;
 }
 
 int main(int argc, char *argv[]) {
@@ -177,17 +166,23 @@ int main(int argc, char *argv[]) {
     int flag = atoi(argv[1]);
     int dimension = atoi(argv[2]);
 
+    int old = dimension;
+
     FILE *file;
 
     file = fopen(argv[3], "r");
+
+    if (dimension > 2) {
+        dimension = padding(dimension);
+    }
 
     int **a = newMatrix(dimension);
     int **b = newMatrix(dimension);
 
     for (int i = 0; i < dimension; i++){
         for(int j =0; j < dimension; j++){
-            if(i < dimension && j< dimension){
-                fscanf(file, "%1d", &a[i][j]);
+            if(i < old && j< old){
+                fscanf(file, "%d", &a[i][j]);
             }
             else{
                 a[i][j]=0;
@@ -197,8 +192,8 @@ int main(int argc, char *argv[]) {
 
     for (int i = 0; i < dimension; i++){
         for(int j =0; j < dimension; j++){
-            if(i < dimension && j< dimension){
-                fscanf(file, "%1d", &b[i][j]);
+            if(i < old && j< old){
+                fscanf(file, "%d", &b[i][j]);
             }
             else{
                 b[i][j]=0;
@@ -206,13 +201,15 @@ int main(int argc, char *argv[]) {
         }
     }
 
+
     int **c = strassen(a, b, dimension);
 
-    // only print the diagonals
+    // only print the diagonals, ignore the paddings
     for (int i = 0; i < dimension; i++){
+        if (c[i][i] == 0)
+            continue;
         printf("%d\n", c[i][i]);
     }
-    printf("\n");
 
     freeMatrix(dimension, a);
     freeMatrix(dimension, b);
